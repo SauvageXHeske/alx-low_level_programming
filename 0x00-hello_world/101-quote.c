12bc16d356436f0dd2c549c2138c33c2c64c64f4
SauvageXHeske <heskenizaam@gmail.com>
2022-06-02 10:53:44 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+fprintf(stderr, "and that piece of art is useful\" - \
+		
+		Dora Korpar, 2015-10-19\n");
 
-	return (1);
+return (1);
+
 }
