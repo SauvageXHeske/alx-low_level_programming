b054375e888f7360fb60c564e1e668b2ce3c79d2
SauvageXHeske <heskenizaam@gmail.com>
2022-06-07 07:47:58 -0700

    i suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.n"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
