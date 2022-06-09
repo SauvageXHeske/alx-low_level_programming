db619f8e154f4fdb84f78c90c65841566e2dd751
SauvageXHeske <heskenizaam@gmail.com>
2022-06-09 09:18:01 -0700

    Task 5 fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,14 @@ void more_numbers(void)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 0)
+			if (y > 9)
 			{
 				_putchar((y / 10) + '0');
 			}
 
 			_putchar((y % 10) + '0');
 		}
-	}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
